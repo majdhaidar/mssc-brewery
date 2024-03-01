5ba1aede7bc1f3cb3d35f562d902b7ca33cdd36d
majdhaidar <haidarmajd@hotmail.com>
2024-03-01 12:46:22 +0200

    Initial project commit. Early course sections

--- src/main/java/haidarspringframework/msscbrewery/service/BeerServiceImpl.java
+++ src/main/java/haidarspringframework/msscbrewery/service/BeerServiceImpl.java
@@ -10,5 +10,6 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDTO getBeerById(UUID beerId) {
         return BeerDTO.builder().id(UUID.randomUUID()).beerName("Almaza").beerStyle("Greeno").upc(2L).build();
+
     }
 }
